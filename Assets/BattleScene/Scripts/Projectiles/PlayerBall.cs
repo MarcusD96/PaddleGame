5633ab9ccbee87fffe2f057debf94fc7d678a250
MarcusD96 <marc_not_listening@hotmail.com>
2020-02-22 16:02:49 -0500

    more optimizations

---
+++ Assets/BattleScene/Scripts/Projectiles/PlayerBall.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerBall : Projectile {
+
+    public Text comboNum;
+    private int COMBO;
+
+    void Start () {
+        if(x == 0) {
+            x = 5;
+        }
+        if(y == 0) {
+            y = 5;
+        }
+        BallStart();
+        tag = "Ball";
+        COMBO = 0;
+        comboNum = FindObjectOfType<Text>();
+        comboNum.text = "COMBO: " + COMBO;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        BallUpdate();
+    }
+
+    void OnCollisionEnter2D (Collision2D collision) {
+        switch(collision.gameObject.tag) {
+            case "Paddle":  //ball hits paddle
+                SoundManager.PlaySound("BallPaddle");
+                constantSpeed = new Vector2(x + COMBO, y + COMBO);
+                break;
+
+            case "BallSideBad": //ball hits enemy side wall
+                SoundManager.PlaySound("BallBad");
+                COMBO = 0;
+                comboNum.text = "COMBO: " + COMBO;
+                constantSpeed = new Vector2(x + COMBO, y + COMBO);
+                break;
+
+            case "BallSide":    //ball hits player side wall
+                SoundManager.PlaySound("BallWall");
+                break;
+            case "Enemy":
+                SoundManager.PlaySound("BallWall");
+                COMBO += 1;
+                comboNum.text = "COMBO: " + COMBO;
+                constantSpeed = new Vector2(x + COMBO, y + COMBO);
+                collision.gameObject.GetComponent<Enemy>().ReduceHealth(1);
+                FindObjectOfType<GaugeBar>().UpdateEnemyHealth(.1f);
+
+                break;
+
+            default:
+                break;
+        }
+    }
+}
