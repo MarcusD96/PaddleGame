0544725278b1ea70c9c7522849e03bdbfdcf0c8c
MarcusD96 <marc_not_listening@hotmail.com>
2020-03-01 13:01:12 -0500

    added more anims

--- Assets/Overworld/Scripts/PlayerMovement.cs
+++ Assets/Overworld/Scripts/PlayerMovement.cs
@@ -9,7 +9,9 @@ public class PlayerMovement : MonoBehaviour {
     public Rigidbody2D rb;
     public Animator animator;
 
-    Vector2 movement;
+    private Vector2 movement;
+
+    private bool up = false, down = false, left = false, right = false;
 
     // Update is called once per frame
     void Update() { //Input
@@ -17,11 +19,36 @@ public class PlayerMovement : MonoBehaviour {
         movement.y = Input.GetAxisRaw("Vertical");
 
         animator.SetFloat("Horizontal", movement.x);
+        if(movement.x > 0) {
+            up = down = left = false;
+            right = true;
+        }
+        if(movement.x < 0) {
+            up = down = right = false;
+            left = true;
+        }
         animator.SetFloat("Vertical", movement.y);
+        if(movement.y < 0) {
+            up = left = right = false;
+            down = true;
+        }
+        if(movement.y > 0) {
+            left = down = right = false;
+            up = true;
+        }
         animator.SetFloat("Speed", movement.sqrMagnitude);
+
+        animator.SetBool("Up", up);
+        animator.SetBool("Down", down);
+        animator.SetBool("Left", left);
+        animator.SetBool("Right", right);
     }
 
     void FixedUpdate() { //Movement Physics
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    void CheckBools() {
+
     }
 }
