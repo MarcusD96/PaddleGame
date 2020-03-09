568656b5db93cc369ec7c13a1cc004d28977a40a
MarcusD96 <marc_not_listening@hotmail.com>
2020-03-09 00:28:58 -0400

    fade in

---
+++ Assets/_Scenes/LevelLoader.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Linq;
+
+public class LevelLoader : MonoBehaviour {
+
+    public Animator transition;
+    public float transtionTime = 1.0f;
+
+    // Update is called once per frame
+    void Update() {
+        if(Input.GetMouseButtonDown(0)) {
+            LoadNextLevel();
+        }
+    }
+
+    void LoadNextLevel() {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadLevel(int levelIndex) {
+        //Play anim
+        transition.SetTrigger("Start");
+
+        //wait
+        yield return new WaitForSeconds(transtionTime);
+
+        //load scene
+        SceneManager.LoadScene(levelIndex);
+    }
+}
