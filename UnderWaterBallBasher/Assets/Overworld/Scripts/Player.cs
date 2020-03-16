ea3c8e4477a5866bfaa28410a5f02912440d91ee
MarcusD96 <marc_not_listening@hotmail.com>
2020-03-16 01:07:17 -0400

    added encounters to overworld

--- UnderWaterBallBasher/Assets/Overworld/Scripts/Player.cs
+++ UnderWaterBallBasher/Assets/Overworld/Scripts/Player.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class Player : MonoBehaviour {
     private void OnTriggerEnter2D(Collider2D collision) {
         if(collision.CompareTag("Finish")) {
+            FindObjectOfType<LevelLoader>().LoadNextLevel(Levels.lobster);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if(collision.gameObject.CompareTag("Encounter")) {
             FindObjectOfType<LevelLoader>().LoadNextLevel(Levels.zombie);
         }
     }
