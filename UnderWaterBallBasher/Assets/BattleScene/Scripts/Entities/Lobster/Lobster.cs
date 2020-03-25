7e4a5d882261353c4c124c16d001236890a0d021
MarcusD96 <marc_not_listening@hotmail.com>
2020-03-24 20:55:31 -0400

    Revert "Merge branch 'master' of https://github.com/MarcusD96/PaddleGame"
    This reverts commit d751ce635d75aa8453ddc82680a9584c1714fb7c, reversing
    changes made to 7ab6336d6b093cbf9c0daf11e6456582a319317b.

--- UnderWaterBallBasher/Assets/BattleScene/Scripts/Entities/Lobster/Lobster.cs
+++ UnderWaterBallBasher/Assets/BattleScene/Scripts/Entities/Lobster/Lobster.cs
@@ -48,7 +48,7 @@ public class Lobster : Enemy {
                 break;
 
             case 3:
-                //noMove = shockwaveAttack.canShoot;
+                noMove = shockwaveAttack.canShoot;
                 if(Time.timeSinceLevelLoad > armNextFire) {
                     armNextFire = Time.timeSinceLevelLoad + armFireRate;
                     shockwaveAttack.canShoot = true;
@@ -74,7 +74,7 @@ public class Lobster : Enemy {
     }
 
     private void ArmFire() {
-        //noMove = fireArm.canShoot;
+        noMove = fireArm.canShoot;
         if(Time.timeSinceLevelLoad > armNextFire) {
             armNextFire = Time.timeSinceLevelLoad + armFireRate;
             fireArm.canShoot = true; //allowed to shoot
