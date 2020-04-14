34b68243484c07de38135b1d7bfab0c66c47daf3
VileContrarian <hibberd213@gmail.com>
2020-04-14 19:46:13 -0400

    Updated TestAI dll to new VS build kit
    Title

--- TestAI/TestAI/TestAI.h
+++ TestAI/TestAI/TestAI.h
@@ -4,14 +4,4 @@
 
 #define TEST_API __declspec(dllexport)
 
-extern "C" TEST_API float Random();
-
-extern "C" TEST_API void Sort(int a[], int length);
-
-extern "C" TEST_API const char* SendHello();
-
-extern "C" TEST_API const char* MakeString(const char* s1, const char* s2);
-
-extern "C" TEST_API float Move();
-
 extern "C" TEST_API float FollowB(float _BY, float _EY, float _Sped);
