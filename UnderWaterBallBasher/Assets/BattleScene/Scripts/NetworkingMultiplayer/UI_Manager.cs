bba30cef511e64d52d39da473c0722dbdfa07111
2000Redred <jfinewax@hotmail.ca>
2020-06-04 19:17:52 -0400

    Networking Practice

---
+++ UnderWaterBallBasher/Assets/BattleScene/Scripts/NetworkingMultiplayer/UI_Manager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Manager : MonoBehaviour
+{
+    public static UI_Manager instance;
+
+    public GameObject startMenu;
+    public InputField usernameField;
+
+    private void Awake() {
+        if (instance == null) {
+            instance = this;
+        }
+        else if (instance != this) {
+            Debug.Log("Instance already exists which destroys the object!");
+            Destroy(this);
+        }
+    }
+
+    public void ConnectToServer() {
+        startMenu.SetActive(false);
+        usernameField.interactable = false;
+        Client.instance.ConnectToServer();
+    }
+}
